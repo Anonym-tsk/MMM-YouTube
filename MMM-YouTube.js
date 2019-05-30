11ee27b4e529391db7a42779b82fbb080bb1d716
eouia <eouia0819@gmail.com>
2019-05-30 03:47:01 +0200

    release

---
+++ MMM-YouTube.js
@@ -0,0 +1,293 @@
+Module.register("MMM-YouTube", {
+  defaults: {
+    playerVars: {
+      controls: 0,
+      hl: "en",
+      enablejsapi: 1,
+      showinfo: 0,
+      rel: 0,
+      cc_load_policy: 0,
+    },
+    defaultQuality: "default",
+    disableCC: true,
+    showPlayingOnly: true,
+    onStartPlay: null,
+    defaultLoop: false,
+    defaultShuffle: false,
+    defaultAutoplay: true,
+    verbose:true,
+    telegramBotCommand: {
+      YOUTUBE_LOAD_BY_URL: "yt",
+      YOUTUBE_CONTROL: "yc"
+    },
+    outNotifications: {
+      "-1": "UNSTARTED",
+      "0": "ENDED",
+      "1": "PLAYING",
+      "2": "PAUSED",
+      "3": "BUFFERING",
+      "5": "VIDEO CUED",
+    }
+  },
+
+  getStyles: function() {
+    return ["MMM-YouTube.css"]
+  },
+
+  start: function() {
+    this.isYoutubeReady = false
+    this.YTPlayer = null
+    this.suspended = false
+    this.list = false
+    this.videoLoop = this.config.defaultLoop
+    this.videoShuffle = this.config.defaultShuffle
+    this.videoAutoplay = this.config.defaultAutoplay
+
+  },
+  getCommands: function(commander) {
+    commander.add({
+      command: this.config.telegramBotCommand.YOUTUBE_LOAD_BY_URL,
+      callback: "T_yt"
+    })
+    commander.add({
+      command: this.config.telegramBotCommand.YOUTUBE_CONTROL,
+      callback: "T_yc"
+    })
+  },
+
+  T_yt: function(command, handler) {
+    if (handler.args) {
+      this.loadVideo({type:"url", id:handler.args, autoplay:true})
+    } else {
+      this.loadVideo(this.config.onStartPlay)
+    }
+  },
+
+  T_yc: function(command, handler) {
+    if (!handler.args) {
+      handler.reply("TEXT", "E: Action Required.")
+      return
+    }
+    var param = handler.args.split(" ")
+    var ret = this.controlPlayer(param[0], param[1])
+    if (ret) handler.reply("TEXT", ret.toString())
+  },
+
+
+
+  getDom: function () {
+    var dom = document.createElement("div")
+    dom.id = "YOUTUBE"
+    if (this.config.showPlayingOnly) dom.style.display = "none"
+    var player = document.createElement("div")
+    player.id = "YOUTUBE_PLAYER"
+    dom.appendChild(player)
+    return dom
+  },
+
+  suspend: function () {
+    this.suspended = true
+    var ret = this.controlPlayer("getPlayerState")
+    if (ret == 1) {
+      this.controlPlayer("pauseVideo")
+    }
+  },
+
+  resume: function () {
+    this.suspended = false
+    var ret = this.controlPlayer("getPlayerState")
+    if (ret == 1) {
+      this.controlPlayer("playVideo")
+    }
+  },
+
+  notificationReceived: function (noti, payload) {
+    if (noti == "DOM_OBJECTS_CREATED") {
+      this.prepare()
+    }
+    if (noti == "YOUTUBE_LOAD") {
+      this.loadVideo(payload)
+    }
+    if (noti == "YOUTUBE_CONTROL") {
+      /*
+        {
+          command: "playVideo",
+          param: {},
+          callback: (ret)=<{}
+        }
+      */
+      var ret = this.controlPlayer(payload.command, payload.param)
+      if (typeof payload.callback == "function") payload.callback(ret)
+    }
+  },
+
+  loadVideo: function(payload) {
+    var option = {}
+    var method = ""
+    if (!payload) return false
+    if (typeof payload.id == "undefined") return false
+    this.list = false
+    if (payload.type == "id") {
+      option = {videoId: payload.id}
+      method = "VideoById"
+    } else if (payload.type == "url") {
+      var regulateURL = (url) => {
+        var regex = new RegExp(/[\/?&]v((=|\/)([^?&#\/]*)|\?|\/|&|#|$)/)
+        var results = regex.exec(url)
+        if (!results) return null
+        if (!results[3]) return ''
+        return decodeURIComponent(results[3].replace(/\+/g, ' '));
+      }
+      var regulated = regulateURL(payload.id)
+      if (regulated) {
+        option = {mediaContentUrl: `http://www.youtube.com/v/${regulated}?version=3`}
+        method = "VideoByUrl"
+      } else {
+        return false
+      }
+
+    } else if (payload.type == "playlist") {
+      this.list = true
+      option = {
+        list: payload.id,
+        listType: (payload.listType) ? payload.listType : "playlist",
+        index: (payload.index) ? payload.index : 0,
+      }
+      method = "Playlist"
+    } else {
+      return false
+    }
+    option.suggestedQuality = this.config.defaultQuality
+
+    var fn = "cue" + method
+    this.videoLoop = (payload.hasOwnProperty("loop"))
+      ? payload.loop : this.config.defaultLoop
+    this.videoShuffle = (payload.hasOwnProperty("shuffle"))
+      ? payload.shuffle : this.config.defaultShuffle
+    this.videoAutoplay = (payload.hasOwnProperty("autoplay"))
+      ? payload.autoplay : this.config.defaultAutoplay
+    this.controlPlayer(fn, option)
+  },
+
+  controlPlayer: function(command, param=null) {
+    if (this.config.verbose) console.log("[YOUTUBE] Control:", command, param)
+    if (!this.YTPlayer || !command) return false
+    if (typeof this.YTPlayer[command] == "function") {
+      var ret = this.YTPlayer[command](param)
+      if (ret && ret.constructor.name == "Y") ret = null
+      return ret
+    }
+  },
+
+  prepare: function() {
+    var tag = document.createElement("script")
+    tag.src = "https://www.youtube.com/iframe_api"
+    var firstScriptTag = document.getElementsByTagName("script")[0]
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+    window.onYouTubeIframeAPIReady = () => {
+      this.isYoutubeReady = true
+      if (this.config.verbose) console.log("[YOUTUBE] API is ready.")
+      this.initPlayer(this.makeYTOptions())
+    }
+  },
+
+  playerOnReady: function(ev) {
+    if (this.config.verbose) console.log("[YOUTUBE] Player is ready.")
+    if (this.config.onStartPlay) {
+      this.loadVideo(this.config.onStartPlay)
+    }
+  },
+
+  playerOnStateChange: function(ev) {
+    if (this.config.verbose) console.log("[YOUTUBE] Status Changed:", ev.data)
+    this.sendNotification(this.config.outNotifications[ev.data])
+    if (this.config.disableCC && ev.data == 1) {
+      ev.target.unloadModule("captions");  //Works for html5 ignored by AS3
+      ev.target.unloadModule("cc");
+    }
+
+    if (this.config.showPlayingOnly) {
+      var dom = document.getElementById("YOUTUBE")
+      if (ev.data == 1) {
+        dom.style.display = "block"
+      } else {
+        dom.style.display = "none"
+      }
+    }
+    if (ev.data == 5) {
+      var id = 0
+      if (this.list) {
+        var list = this.controlPlayer("getPlaylist")
+        if (!Array.isArray(list)) return false
+        if (this.config.verbose) console.log("[YOUTUBE] Playlist count:", list.length)
+        if (list.length > 0) {
+          if (this.videoShuffle) {
+            id = Math.floor(Math.random()*list.length)
+          }
+          this.controlPlayer("setShuffle", this.videoShuffle)
+        }
+      }
+      this.controlPlayer("setLoop", this.videoLoop)
+      if (this.videoAutoplay) {
+        if (id > 0) {
+          this.controlPlayer("playVideoAt", id)
+        } else {
+          this.controlPlayer("playVideo")
+        }
+      }
+    }
+  },
+
+  playerOnError: function(ev) {
+    var kind = "Unknown Error"
+    switch(ev.data) {
+      case 2 :
+        kind = "Invalid Parameter"
+        break
+      case 5 :
+        kind = "HTML5 Player Error"
+        break
+      case 100 :
+        kind = "Video Not Found (removed or privated)"
+        break
+      case 101 :
+      case 150 :
+        kind = "Not Allowed By Owner"
+        break
+      default:
+        break
+    }
+    if (this.config.verbose) console.log(`[YOUTUBE] Player Error: (${ev.data})`, kind)
+    if (ev.data == 2) {
+      ev.target.stopVideo()
+    }
+  },
+
+  makeYTOptions: function(options={}) {
+    if (options.hasOwnProperty("playerVars")) {
+      options.playerVars = Object.assign({}, this.config.playerVars, options.playerVars)
+    } else {
+      options.playerVars = Object.assign({}, this.config.playerVars)
+    }
+
+    options = Object.assign({}, this.config.defaultPlayerOptions, options)
+    options.events = {}
+    options.events.onReady = (ev) => {
+      this.playerOnReady(ev)
+    }
+    options.events.onStateChange = (ev) => {
+      this.playerOnStateChange(ev)
+    }
+    options.events.onError = (ev) => {
+      this.playerOnError(ev)
+    }
+    options.events.onAPIChanged = (ev) => {
+      //nothing to do...
+    }
+    return options
+  },
+
+  initPlayer: function(options) {
+    this.YTPlayer = new YT.Player("YOUTUBE_PLAYER", options)
+  }
+})
--- README.md
+++ README.md
@@ -1,2 +1,197 @@
-# MMM-YoutubePlayer
-MagicMirror module for youtube player
+# MMM-YouTube
+MagicMirror module for youtube player with runtime configuration and controllable by notification.
+
+## Feature
+- Embedded YouTube Player on MagicMirror
+- Load and Play videos dynamically on runtime, not only statically by configuration.
+- Almost every YouTube iFrame APIs be supported.
+- Controllable by notification and callback
+- Controllable by MMM-TelegramBot command
+
+## Installation
+```js
+cd ~/MagicMirror/modules
+git clone https://github.com/eouia/MMM-YouTube
+```
+
+## Configuration
+### Simple Version (Just being ready)
+```js
+{
+  disabled:disabled["MMM-YouTube"],
+  module: "MMM-YouTube",
+  position: "top_right",
+},
+```
+
+### Defaults & Details
+You don't need to copy & paste all of these. Just select what you need and rewrite it into your `config.js`. Other values will be applied automatically.
+```js
+{
+  disabled:disabled["MMM-YouTube"],
+  module: "MMM-YouTube",
+  position: "top_right",
+  config: {
+    verbose:true,
+    defaultQuality: "default",
+    disableCC: true,
+    showPlayingOnly: true,
+    defaultLoop: false,
+    defaultShuffle: false,
+    defaultAutoplay: true,
+    onStartPlay: null,
+    playerVars: {
+      controls: 0,
+      hl: "en",
+      enablejsapi: 1,
+      showinfo: 0,
+      rel: 0,
+      cc_load_policy: 0,
+    },
+    telegramBotCommand: {
+      YOUTUBE_LOAD_BY_URL: "yt",
+      YOUTUBE_CONTROL: "yc"
+    },
+    outNotifications: {
+      "-1": "UNSTARTED",
+      "0": "ENDED",
+      "1": "PLAYING",
+      "2": "PAUSED",
+      "3": "BUFFERING",
+      "5": "VIDEO CUED",
+    }
+  }
+},
+```
+#### verbose: true
+If you set `false`, front-end log will be hidden. When you need to debug, set this field as `true`
+
+#### defaultQuality: "default"
+The parameter value can be `small`, `medium`, `large`, `hd720`, `hd1080`, `highres` or `default`. I recommend that you set the parameter value to `default`, which instructs YouTube to select the most appropriate playback quality, which will vary for different users, videos, systems and other playback conditions.
+And even if you set other value, it doesn't mean the quality is guaranteed on playing. It could be changed by YouTube by force. Usually, Quality could be changed by player size, network speed, computing power and etc.
+
+> more : https://developers.google.com/youtube/iframe_api_reference#Playback_quality
+
+#### disableCC: true
+Caption will not be shown with `true`
+
+#### showPlayingOnly: true
+with `true`, This module will be appeared only when it is played.
+
+#### defaultLoop: false,
+Set default looping status of video(s)
+
+#### defaultShuffle: false,
+Set default shuffle status of playlist. This will be applied only when playlist be loaded.
+
+#### defaultAutoplay: true,
+Set default autoplay status. If this is set as `false`, video will be loaded but not played automatically.
+
+#### playerVars: {}
+Set environment of player.
+
+Some of these parameters might not be working due to YouTube API's limitation.
+
+> more: https://developers.google.com/youtube/player_parameters#Parameters
+
+
+#### onStartPlay: {}
+If you want to play a video from startup of MagicMirror, set the video info here. OR if you don't want, just set as `null`
+```js
+onStartPlay: {
+  type: "id",
+  id: "UOxkGD8qRB4",
+  shuffle: true,
+  loop: false,
+  autoplay: true,
+}
+```
+See `VIDEO_LOAD` section also.
+
+#### telegramBotCommand: {}
+You can redefine telegramBot commands for this module. Default values are
+- `/yt` : load video
+- `/yc` : control video
+
+#### outNotifications: {}
+When player status be changed, one of `outNotifications` will be emitted.
+
+I believe you don't need to modify this. You can redefine notification message with this.
+
+
+## TelegramBot Commands
+You can load and control video with MMM-TelegramBot
+- `/yt YOUTUBE_URL` : load specific Youtube clip on MagicMirror and play it.
+- `/yc CONTROL_COMMAND` : control video. (e.g: `/yc pauseVideo`)
+See `Control Commands` section for Available control commands
+
+## Control by notification
+You can control video by notification from other module.
+e.g)
+```js
+this.sendNotification("YOUTUBE_LOAD", {type:"id", id:"UOxkGD8qRB4"})
+```
+### YOUTUBE_LOAD
+You can use one of 3 types for loading YouTube video. And these object structure could be used `onStartPlay`.
+#### type:id
+```js
+this.sendNotification("YOUTUBE_LOAD", {
+  type: "id", //REQUIRED
+  id: "UOxkGD8qRB4", //REQUIRED. YouTube Video Id.
+  loop: false, //OPTIONAL
+  autoplay: true, //OPTIONAL
+})
+```
+#### type:url
+```js
+this.sendNotification("YOUTUBE_LOAD", {
+  type: "url", //REQUIRED
+  id: "https://www.youtube.com/watch?v=UOxkGD8qRB4", //REQUIRED. YouTube Video URL
+  loop: false, //OPTIONAL
+  autoplay: true, //OPTIONAL
+})
+```
+#### type:playlist
+```js
+this.sendNotification("YOUTUBE_LOAD", {
+  type: "playlist", //REQUIRED
+  listType: "playlist", //REQUIRED. "playlist", "search", "user_uploads" be available.
+  id: "PL4QNnZJr8sRNKjKzArmzTBAlNYBDN2h-J", //REQUIRED. See description
+  index: 0, // OPTIONAL.
+  shuffle: false, //OPTIONAL
+  loop: false, //OPTIONAL
+  autoplay: true, //OPTIONAL
+})
+```
+- `listType:"playlist"` : `id` should be YouTube Playlist Id. (e.g: PL4QNnZJr8sRNKjKzArmzTBAlNYBDN2h-J)
+- `listType:"search"` : `id` should be search terms. (e.g: "Michael Jackson")
+- `listType:"user_uploads"` : `id` should be user id (e.g: "eouia0819")
+
+
+### YOUTUBE_CONTROL
+You can use most of YouTube iFrame API methods to control the player.
+```js
+this.sendNotification("YOUTUBE_CONTROL", {
+  command: "playVideo"
+})
+
+this.sendNotification("YOUTUBE_CONTROL", {
+  command: "setVolume",
+  params: "60",
+})
+
+this.sendNotification("YOUTUBE_CONTROL", {
+  command: "getVolume",
+  callback: (ret) => {
+    console.log("Current volume is", ret)
+  }
+})
+```
+Each control command could have these fields.
+- `command` : REQUIRED. You can use almost all of YouTube iFrame API
+- `param`: OPTIONAL. When parameter is required for the API
+- `callback`: OPTIONAL. This callback function will be called after command with result.
+
+#### Available commands
+`playVideo`, `pauseVideo`, `stopVideo`, `nextVideo`, `previousVideo`, `mute`, `unMute`, ...
+See https://developers.google.com/youtube/iframe_api_reference#Functions
